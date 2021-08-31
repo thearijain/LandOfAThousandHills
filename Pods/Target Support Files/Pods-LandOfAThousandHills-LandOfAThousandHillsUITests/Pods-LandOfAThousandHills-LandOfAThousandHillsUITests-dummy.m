0e30be34ecbaf890c7b0b34a6dda300543745d12
Ari Jain <thearijain@gmail.com>
2021-08-31 17:21:58 -0400

    Delete Pods directory

--- Pods/Target Support Files/Pods-LandOfAThousandHills-LandOfAThousandHillsUITests/Pods-LandOfAThousandHills-LandOfAThousandHillsUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LandOfAThousandHills_LandOfAThousandHillsUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LandOfAThousandHills_LandOfAThousandHillsUITests
-@end
--- Pods/Target Support Files/Pods-LandOfAThousandHills/Pods-LandOfAThousandHills-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LandOfAThousandHills : NSObject
-@end
-@implementation PodsDummy_Pods_LandOfAThousandHills
-@end
